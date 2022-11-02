f0858db8e16501657065624d0904d0e86fd8c21d
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2022-11-02 07:17:10 +0100

    made some css changes

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -70,11 +70,12 @@ export default function SignIn({ providers }) {
     const { error } = res;
     if (error) {
       setLoading(false);
+      toast(error, { type: 'error' });
       setSignupError(error);
       return;
     }
     setLoading(false);
-    toast('Signin Successful!', { type: 'success' });
+    toast('Signup Successful!', { type: 'success' });
     // redirect to sigin page
     router.push('/api/auth/signin');
   };
@@ -88,7 +89,7 @@ export default function SignIn({ providers }) {
       <div className="h-screen flex flex-col justify-center items-center">
         <form onSubmit={handleCredentialsSubmit} method="post" className="w-4/5 sm:w-96 md:lg-1/3 lg:w-1/4">
           <h1 className="text-center my-5 text-3xl">Please Signup </h1>
-          {signupError && <p className="text-center">{signupError}</p>}
+          {signupError && <p className="text-center text-red-500">{signupError}</p>}
           <div>
             <label htmlFor="username">Username</label>
             <input onChange={handleFormInput} placeholder="johndoe123" name="username" className="mt-2 dark:text-black  block border w-full p-2 rounded-md" />
@@ -109,7 +110,7 @@ export default function SignIn({ providers }) {
             <input onChange={handleFormInput} placeholder="***********" name="password" type="password" className="mt-2 dark:text-black block border w-full p-2 rounded-md" />
             {errors.password && <p className="text-red-400">{errors.password}</p>}
           </div>
-          {loading ? <Loading /> : <button type="submit" className="bg-black w-20 text-white p-2 rounded-md mt-5">Sign up</button>}
+          {loading ? <Loading className="border" /> : <button type="submit" className="bg-black w-20 border text-white p-2 rounded-md mt-5">Sign up</button>}
         </form>
         <div className="w-4/5 sm:w-96 md:lg-1/3 lg:w-1/4 mt-5">
 
